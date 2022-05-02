10052f67cc04dadb71ace8d49e8a88152ed29f5c
Liam Carnahan <66342321+carnahanliam@users.noreply.github.com>
2022-05-02 13:41:28 -0400

    update baseurl

--- ledger-frontend/src/services/login.js
+++ ledger-frontend/src/services/login.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = '/api/login'
+const baseUrl = 'https://transaction-splitter-app.herokuapp.com/api/login'
 
 const loginUser = async (credentials) => {
   const response = await axios.post(baseUrl, credentials)
